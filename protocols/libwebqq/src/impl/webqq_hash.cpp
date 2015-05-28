fe673520e2ea21f0c010c106115577ce72fd1f04
jack <jack.wgm@gmail.com>
2015-05-29 02:41:54 +0800

    fix hash

--- protocols/libwebqq/src/impl/webqq_hash.cpp
+++ protocols/libwebqq/src/impl/webqq_hash.cpp
@@ -46,11 +46,13 @@ namespace detail {
 
 std::string hash_func(std::string x, std::string K)
 {
-	if (hash_func_content == R"javascript(function hash(x,K)\n\{x+="";for(var N=[],T=0;T<K.length;T++)N[T%4]^=K.charCodeAt(T);var U=["EC","OK"],V=[];V[0]=x>>24&255^U[0].charCodeAt(0);V[1]=x>>16&255^U[0].charCodeAt(1);V[2]=x>>8&255^U[1].charCodeAt(0);V[3]=x&255^U[1].charCodeAt(1);
+	if (hash_func_content == R"javascript(function hash(x,K)
+{x+="";for(var N=[],T=0;T<K.length;T++)N[T%4]^=K.charCodeAt(T);var U=["EC","OK"],V=[];V[0]=x>>24&255^U[0].charCodeAt(0);V[1]=x>>16&255^U[0].charCodeAt(1);V[2]=x>>8&255^U[1].charCodeAt(0);V[3]=x&255^U[1].charCodeAt(1);
 U=[];for(T=0;T<8;T++)U[T]=T%2==0?N[T>>1]:V[T>>1];N=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];V="";for(T=0;T<U.length;T++){V+=N[U[T]>>4&15];V+=N[U[T]&15]}return V})javascript")
 	{
 		return hash_p(x.c_str(), K.c_str());
 	}
+
 	return hash_auto(x, K);
 }
 
@@ -124,7 +126,7 @@ static void extract_hash_function(boost::system::error_code& ec,  std::string md
 		finded_location++;
 	}while(*finded_location!='{');
 
-	hash_func_content += "\n\{";
+	hash_func_content += "\n{";
 
 	int indent_code_block = 1;
 
